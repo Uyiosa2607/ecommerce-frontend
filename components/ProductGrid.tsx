0a4ee7feaf23f9602d94e96aa31f6be0b05987da
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-01-08 20:39:33 +0100

    improvements made

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -46,6 +46,7 @@ export default function ProductGrid({ products, loading }: ProductGridProps) {
                       alt={product.name}
                       width={300}
                       height={300}
+                      quality={100}
                       className="w-full h-[200px] object-cover transition-transform duration-300 group-hover:scale-105"
                     />
                     <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-10 transition-opacity duration-300" />
