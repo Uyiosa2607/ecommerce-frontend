295a296ac2d660662d30dc276d3b024e3da27c33
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-04-07 00:17:16 +0100

    made changes

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,18 +1,18 @@
-"use client";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import ProductDetails from "@/components/ProductDetails";
-import { useParams } from "next/navigation";
+// "use client";
+// import Header from "@/components/Header";
+// import Footer from "@/components/Footer";
+// import ProductDetails from "@/components/ProductDetails";
+// import { useParams } from "next/navigation";
 
-export default function ProductPage() {
-  const params = useParams();
-  return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-gray-50 to-gray-100">
-      <Header />
-      <main className="flex-grow container mx-auto px-4 py-12">
-        <ProductDetails id={String(params.id)} />
-      </main>
-      <Footer />
-    </div>
-  );
-}
+// export default function ProductPage() {
+//   const params = useParams();
+//   return (
+//     <div className="min-h-screen flex flex-col bg-gradient-to-br from-gray-50 to-gray-100">
+//       <Header />
+//       <main className="flex-grow container mx-auto px-4 py-12">
+//         <ProductDetails id={String(params.id)} />
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// }
